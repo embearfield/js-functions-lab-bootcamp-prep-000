7a62ea5b1d097cfd0696021b0fced3dbab6d0601
Eric Bearfield <embearfield@gmail.com>
2017-11-27 19:51:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ' + holiday + ', ' + Name + '!'
+  return 'Happy ' + holiday + ', ' + name + '!'
 }
 
 function holidayCountdown(days, holiday) {
